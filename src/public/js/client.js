950f73b1de9571de382bdee3e660b84900725bcb
perezfr <francisco.perez@ucdenv.edu>
2016-11-29 17:33:59 -0700

    Added chart js and divs

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -133,8 +133,83 @@
     }, LOOP_MS);
   }
  
-
-  $(document).ready(function() {   
+  $(document).ready(function() {
+    Charts.initCharts();
+    // chart1 = new Highcharts.Chart({
+    //   chart: {
+    //     renderTo: 'chart-1',
+    //     backgroundColor: {
+    //       linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+    //       stops: [
+    //         [0, 'rgb(255, 255, 255)'],
+    //         [1, 'rgb(153, 255, 204)']
+    //       ]
+    //     },
+    //     defaultSeriesType: 'spline',
+    //     events: {
+    //       //load: requestData
+    //     }
+    //   },
+    //   title: {
+    //     text: 'CPU Usage'
+    //   },
+    //   xAxis: {
+    //     type: 'datetime',
+    //     tickPixelInterval: 150,
+    //     maxZoom: 20 * 1000
+    //   },
+    //   yAxis: {
+    //     minPadding: 0.2,
+    //     maxPadding: 0.2,
+    //     title: {
+    //       text: 'Percentage',
+    //       margin: 80
+    //     }
+    //   },
+    //   series: [{
+    //     name: 'Host',
+    //     data: [],
+    //     color: 'black'
+    //   },{
+    //     name: 'Container',
+    //     data: [],
+    //     color: 'blue'
+    //   }]
+    // });
+    // chart2 = new Highcharts.Chart({
+    //   chart: {
+    //     renderTo: 'chart-2',
+    //     defaultSeriesType: 'spline',
+    //     backgroundColor: {
+    //       linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+    //       stops: [
+    //         [0, 'rgb(255, 255, 255)'],
+    //         [1, 'rgb(178, 255, 102)']
+    //       ]
+    //     },
+    //   },
+    //   title: {
+    //     text: 'CPU Usage Variance'
+    //   },
+    //   xAxis: {
+    //     type: 'datetime',
+    //     tickPixelInterval: 150,
+    //     maxZoom: 20 * 1000
+    //   },
+    //   yAxis: {
+    //     minPadding: 0.2,
+    //     maxPadding: 0.2,
+    //     title: {
+    //       text: 'Percantage',
+    //       margin: 80
+    //     }
+    //   },
+    //   series: [{
+    //     name: 'Variance',
+    //     data: [],
+    //     color: 'orange'
+    //   }]
+    // });   
     loadInitialContainerIds(() => {
       $('#polling button').click(function(){
         if($(this).hasClass('locked_active') || $(this).hasClass('unlocked_inactive')){
